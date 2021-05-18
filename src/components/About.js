ccba433ef4593790c8ed49b95e862d35f249eabf
Yevgeniy Valdman <evaldman@yahoo.com>
2021-05-18 13:07:37 -0400

    about

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,28 @@
 import React from "react";
+import profile from "../profile-pic.jpg";
 
 function About() {
-  return <div>About me</div>;
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-6 col-xm-12">
+          <div className="photo-wrap mb-5">
+            <img className="profile-img" src={profile} alt="profile picture" />
+          </div>
+        </div>
+        <div className="col-lg-6 col-xm-12">
+          <h1 className="about-heading">about me</h1>
+          <p>
+            Full stack web developer with experience in Ruby on Rails,
+            Javascript and React. Interested in designing creative and engaging
+            user experiences. With a background in auditing, hospitality and a
+            passion for puzzles, I bring strong skills in client relations,
+            attention to detail and problem solving.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
